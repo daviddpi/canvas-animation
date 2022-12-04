65d295d7806780295f38394d647db426b67669e1
daviddpi <piscopodavid97@gmail.com>
2022-12-04 17:20:02 +0100

    animation texture wallpaper canvas

--- script.js
+++ script.js
@@ -52,6 +52,8 @@ class FlowFieldEffect{
         this.gradient;
         this.#createGradient();
         this.#ctx.strokeStyle = this.gradient;
+        this.radius = 0;
+        this.vr = 0.03;
     }
 
     #createGradient(){
@@ -70,7 +72,7 @@ class FlowFieldEffect{
         
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(x + Math.cos(angle) * 10 , y + Math.sin(angle) * 10);
+        this.#ctx.lineTo(x + Math.cos(angle) * 30 , y + Math.sin(angle) * 30);
         this.#ctx.stroke();
     }
 
@@ -83,11 +85,13 @@ class FlowFieldEffect{
 
             // this.angle += 0.1;
             this.#ctx.clearRect(0, 0, this.#width, this.#height);
-
-
+            this.radius += this.vr;
+            if(this.radius > 5 || this.radius < -5){
+                this.vr *= -1;
+            }
             for(let y = 0; y < this.#height; y += this.cellSize){
                 for(let x = 0; x < this.#width; x += this.cellSize){
-                    const angle = Math.cos(x * 0.05) + Math.sin(y * 0.05);
+                    const angle = (Math.cos(x * 0.01) + Math.sin(y * 0.01)) * this.radius;
                     this.#drawLine(angle, x, y);
                 }
             }
